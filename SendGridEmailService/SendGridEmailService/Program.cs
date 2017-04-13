275aed35622f0d57d0cfe42c256118199019082b
Nilesh Patil <nileshpatil.work@gmail.com>
2017-04-13 23:45:41 +0530

    updated framework

--- SendGridEmailService/SendGridEmailService/Program.cs
+++ SendGridEmailService/SendGridEmailService/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SendGridEmailService
 {
@@ -6,7 +10,7 @@ namespace SendGridEmailService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
             Console.ReadKey(true);
         }
     }
